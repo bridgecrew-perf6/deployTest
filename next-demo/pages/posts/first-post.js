bbacfd7243a28f5cf9e871e75d37a38018ffc28e
priyanka <59654906+Priyanka-Mahepal@users.noreply.github.com>
2022-06-22 18:35:14 +0530

    Add files via upload

---
+++ next-demo/pages/posts/first-post.js
@@ -0,0 +1,31 @@
+import Link from 'next/link';
+import Head from 'next/head';
+import Image from 'next/image';
+import Script from 'next/script';
+import Layout from '../../Components/layout';
+export default function FirstPost() {
+  return (
+    <Layout>
+    <Head>
+      <title>First Post</title>
+    </Head>
+    <Script
+        src="https://connect.facebook.net/en_US/sdk.js"
+        strategy="lazyOnload"
+        onLoad={() =>
+          console.log(`script loaded correctly, window.FB has been populated`)
+        }
+      />
+      <h1>First Post</h1>
+      <h2>
+        <Link href="/">
+          <a>Back to home</a>
+        </Link>
+        
+      </h2>
+      <Image src="/images/profile.jpg" height={144} 
+    width={144} 
+    alt="Your Name" />
+    </Layout>
+  );
+}
