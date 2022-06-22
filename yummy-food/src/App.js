d61f269419e922aaf12253c6fc3d1d995a78d9dc
priyanka <59654906+Priyanka-Mahepal@users.noreply.github.com>
2022-06-22 18:27:27 +0530

    Update App.js

--- yummy-food/src/App.js
+++ yummy-food/src/App.js
@@ -11,30 +11,30 @@ import Footer from './components/Footer'
 import scrollreveal from "scrollreveal";
 
 export default function App() {
-  useEffect(() => {
-    const sr = scrollreveal({
-      origin: "top",
-      distance: "80px",
-      duration: 2000,
-      reset: false,
-    });
-    sr.reveal(
-      `
-        nav,
-        #home,
-        #services,
-        #portfolio,
-        #testimonials,
-        #products,
-        #newsletter,
-        .footer
-    `,
-      {
-        opacity: 0,
-        interval: 200,
-      }
-    );
-  }, []);
+//   useEffect(() => {
+//     const sr = scrollreveal({
+//       origin: "top",
+//       distance: "80px",
+//       duration: 2000,
+//       reset: false,
+//     });
+//     sr.reveal(
+//       `
+//         nav,
+//         #home,
+//         #services,
+//         #portfolio,
+//         #testimonials,
+//         #products,
+//         #newsletter,
+//         .footer
+//     `,
+//       {
+//         opacity: 0,
+//         interval: 200,
+//       }
+//     );
+//   }, []);
   return (
     <>
       <ScrollToTop />
